397a482ebfbe0b2f50114babd1b3e38f05de7f53
Izel Bikulova <izel_bikulova@mail.ru>
2025-05-13 15:31:47 +0300

    connecting to the backend

--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/graduation_paper/" : "/",
-  
-  // Добавляем настройки для решения проблемы Invalid Host header
-  devServer: {
-    allowedHosts: 'all',
-    client: {
-      webSocketURL: 'auto://0.0.0.0:0/ws'
-    },
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  }
+
+
 })
 
